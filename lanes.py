8938dffc00c451e5cdc7e006b5b17e4abc0a3d52
Muhammad Abid <abidaks@users.noreply.github.com>
2020-03-03 17:13:27 +0500

    added some comments to code

--- lanes.py
+++ lanes.py
@@ -4,6 +4,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+# This function is used to define edges in the image which are used to detect lanes
 def define_edges(image):
 	#converting the image from 3-dimension color image to 1-dimension image for furthur processing
 	gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -13,14 +14,17 @@ def define_edges(image):
 	canny_image = cv2.Canny(blur_image, 50, 150)
 	return canny_image
 
+# This function identify the area of interest in an image where we need to focus.
 def identify_area_of_interest(image):
 	height = image.shape[0]
+	#currently coordinates are defined manually which need to be detected automatically
 	polygons = np.array([[(200, height), (1000, height), (600, 300)]])
 	mask_image = np.zeros_like(image)
 	cv2.fillPoly(mask_image, polygons, 255)
 	masked_image = cv2.bitwise_and(mask_image, image)
 	return masked_image
 
+# This function converts slope and intercept to coordinates of the line.
 def make_coordinates(image, line_parameters):
 	slope, intercept = line_parameters
 	y1 = image.shape[0]
@@ -29,7 +33,8 @@ def make_coordinates(image, line_parameters):
 	x2 = int((y2 - intercept)/slope)
 	return np.array([x1, y1, x2, y2])
 
-
+# There were multiple lines detecting on right side and left side of the car.
+# This function combines multiple lines and show only one line on left and one on right side of the car.
 def slope_intercept_avg(image, lines):
 	left_lines = []
 	right_lines = []
@@ -53,6 +58,7 @@ def slope_intercept_avg(image, lines):
 
 	return np.array([left_line, right_line])
 
+# This function is drawing lines on a blank image drived from the given image
 def display_lines(image, lines):
 	lines_image = np.zeros_like(image)
 	if lines is not None:
@@ -69,12 +75,13 @@ cap = cv2.VideoCapture("data/lane_video.mp4")
 while(cap.isOpened()):
 	_, normal_image = cap.read()
 	edged_image = define_edges(normal_image)
-	interested_area_image = identify_area_of_interest(edged_image);
+	interested_area_image = identify_area_of_interest(edged_image)
+	# We are detecting multiple lines in the region of interest using openCV HoughLinesP function.
 	straight_lines = cv2.HoughLinesP(interested_area_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
 	average_lines = slope_intercept_avg(normal_image, straight_lines)
 	image_with_lines = display_lines(normal_image, average_lines)
 	combined_image = cv2.addWeighted(normal_image, 0.8, image_with_lines, 1, 1)
-	cv2.imshow("lanes identify video", combined_image)
+	cv2.imshow("Lanes Identified Video", combined_image)
 	if cv2.waitKey(1) == ord("b"):
 		break
 cap.release()
